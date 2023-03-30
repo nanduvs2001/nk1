de08838b033b0a7cb9f0912584d0bdd1e67adebe
Nandu <nanduvs2001@gmail.com>
2023-03-30 11:54:02 +0530

    commit1

---
+++ nandu.java
@@ -0,0 +1,11 @@
+public class nandu {
+    public static void name(String[] argd) {
+        System.out.println("hi");
+        System.out.println("How are you");
+        
+    }
+}
+    
+}
+    
+}
